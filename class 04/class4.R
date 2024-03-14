555161c14fd568f325b616aedf7a143fee3d159a
Irene Hsieh <chsieh@ucsd.edu>
2024-03-14 10:48:15 -0700

    Class 4 and 5

---
+++ class 04/class4.R
@@ -0,0 +1,8 @@
+# My first R Script 
+
+x <- 1:50
+plot(x, sin(x))
+plot(x, sin(x), typ="l")
+plot(x, sin(x), typ="l", col="blue")
+plot(x, sin(x), typ="l", col="blue", lwd=2)
+plot(x, sin(x), typ="l", col="red", lwd=2, xlab="Hello")
